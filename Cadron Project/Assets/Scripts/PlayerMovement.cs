916089f3fc0a3c934fcc6a433963eaad0d7c4a3f
Amelia Jones <jonesas@hendrix.edu>
2023-04-12 23:21:38 -0500

    Player with movement & followcam
    Used a panda sprite as a placeholder

---
+++ Cadron Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+     private Rigidbody2D body;
+    public float horizontal;
+    public float vertical;
+
+    private float moveLimiter = 0.7f;
+
+    public float runSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() {
+        if (horizontal != 0 && vertical != 0) {
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+    
+}
