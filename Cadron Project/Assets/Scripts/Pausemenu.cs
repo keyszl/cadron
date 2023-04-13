14e1fa42a3ec180129195b6451c9524f884e3243
Kaden Franklin <kadenfranklin@hotmail.com>
2023-04-12 21:17:53 -0500

    Pause Menu & minor changes

---
+++ Cadron Project/Assets/Scripts/Pausemenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pausemenu : MonoBehaviour
+{
+    // Following this tutorial : https://www.youtube.com/watch?v=JivuXdrIHK0
+
+    public static bool IsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+
+    public void Journal()
+    {
+        Debug.Log("Loading Journal");
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
