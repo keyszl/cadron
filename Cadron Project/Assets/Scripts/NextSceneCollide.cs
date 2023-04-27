72d796c9ba6d2d5a3d485ebbb26f60e1c0173048
Amelia Jones <jonesas@hendrix.edu>
2023-04-27 15:30:42 -0500

    Adjusted NextSceneCollide and Lydia's dialogue

--- Cadron Project/Assets/Scripts/NextSceneCollide.cs
+++ Cadron Project/Assets/Scripts/NextSceneCollide.cs
@@ -7,14 +7,18 @@ public class NextSceneCollide : MonoBehaviour
 {
     public GameObject target;
     public string scene;
+    public string requiredletter;
 
     public void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject == target)
         {
+            if((requiredletter != null) && GameManager.Instance.IsLetterDelivered(requiredletter))
+            {
             //print to the console
             Debug.Log("Player has exited the trigger");
             GameManager.Instance.ChangeScene(scene);
+            }
         }
     }
 
